d9d6f88aa796a1e332405c1b69cb112eee4933f7
N3002 User <N3002user@N3002.uc-bcf.edu.ph>
2024-02-21 09:39:36 +0800

    tried adding array in product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,37 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+{
+$gadgets = [
+[
+'name' => 'Smartphone',
+'brand' => 'Samsung',
+'price' => 799.99,
+],
+[
+'name' => 'Laptop',
+'brand' => 'Apple',
+'price' => 1299.99,
+],
+[
+'name' => 'Tablet',
+'brand' => 'Microsoft',
+'price' => 599.99,
+],
+[
+'name' => 'Smartwatch',
+'brand' => 'Fitbit',
+'price' => 199.99,
+],
+[
+'name' => 'Camera',
+'brand' => 'Canon',
+'price' => 599.99,
+],
+];
+
+return response()->json($gadgets);
 }
+}
+
